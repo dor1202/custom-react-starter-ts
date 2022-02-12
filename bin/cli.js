7e1f8f2ea4fc989a4af1349c453adff8475b0efe
Dor <schreiberdor@gmail.com>
2022-02-12 16:57:22 +0200

    Fix cli command

--- bin/cli.js
+++ bin/cli.js
@@ -16,8 +16,8 @@ const runCommand = command => {
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/dor1202/custom-react-starter-ts ${repoName}`;
 const removeGitCommand = `cd ${repoName} && rm -rf .git`;
-const removeBinCommand = `rm -r ${repoName}/bin`;
 const installDepsCommand = `cd ${repoName} && npm install`;
+const removeBinCommand = `rm -r ${repoName}/bin`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 
@@ -28,12 +28,12 @@ console.log(`Removing git from ${repoName}`);
 const removeGit = runCommand(removeGitCommand);
 if(!removeGit)process.exit(-1);
 
-console.log(`Removing bin folder from ${repoName}`);
-const removeBin = runCommand(removeBinCommand);
-if(!removeBin)process.exit(-1);
-
 console.log(`Installing dependencies for ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
 if(!installedDeps)process.exit(-1);
+
+console.log(`Removing bin folder from ${repoName}`);
+const removeBin = runCommand(removeBinCommand);
+if(!removeBin)process.exit(-1);
 
 console.log(`Congrats! You'r ready to start coding`);
